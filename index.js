9f64667ae86804ffe604444246e715d3a6d332b1
tuantu17121999 <buituantu171299@gmail.com>
2024-10-22 22:36:45 +0700

    sua ten

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get('/',(req,res) =>{
-    res.send('Hello');
+    res.send('LVTN Bùi Tuấn Tú');
 })
 
 app.listen(3000,  () => console.log('listen on port 3000'))
