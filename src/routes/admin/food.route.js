35d0b922916f13749986d9b10f90db9f929cfa64
Bui Tuan Tu <buituantu171299@gmail.com>
2024-11-09 13:52:55 +0700

    search san pham

--- src/routes/admin/food.route.js
+++ src/routes/admin/food.route.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const foodController = require('../../app/controllers/admin/food.controller');
-const upload = require('../../app/middlewares/multer')
+const foodController = require("../../app/controllers/admin/food.controller");
+const upload = require("../../app/middlewares/multer");
 
+router.get("/index", foodController.getAll);
 
-router.get('/index', foodController.getAll);
+router.get("/create", foodController.create);
+router.post("/store", upload.single("image"), foodController.store);
 
-router.get('/create', foodController.create);
-router.post('/store', upload.single('image'), foodController.store);
+router.get("/:id/edit", foodController.updateForm);
+router.put("/:id/update", upload.single("image"), foodController.update);
 
-router.get('/:id/edit', foodController.updateForm);
-router.put('/:id/update', upload.single('image'), foodController.update);
+router.delete("/:id", foodController.delete);
 
-router.delete('/:id', foodController.delete);
-
-
-
+router.get("/search", foodController.search);
 
 module.exports = router;
