8a8d1451c3bead55a1f743124dec6d2a9a0b8a3a
tuantu17121999 <buituantu171299@gmail.com>
2025-01-03 09:47:49 +0700

    sua loi secret

--- src/config/passport/passport.js
+++ src/config/passport/passport.js
@@ -10,11 +10,6 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-
-      // clientID: "803563391879-011pu6ma02dc2ce2jd3sbaup51svdqgg.apps.googleusercontent.com",
-      // clientSecret: "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",
-      // callbackURL: "http://localhost:3000/customer/login/google/callback",
-      // ACCESS_TOKEN_CUSTOMER_SECRE = 'ABC'
     },
     async (accessToken, refreshToken, profile, done) => {
       //get the user data from google
